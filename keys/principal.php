67fc81463b66e37047237b8b0f31c07687e1f5d9
Marcos Farabollini <marcos.farabollini@gmail.com>
2012-11-01 17:05:48 -0300

    upd 1/10

---
+++ keys/principal.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Claves</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="assets/css/site.css" rel="stylesheet">       
+    
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+  <body>
+
+	<div class="navbar">
+	  <div class="navbar-inner">
+	    <div class="container">
+	 
+	      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+	      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </a>
+	 
+	      <!-- Be sure to leave the brand out there if you want it shown -->
+	      <a class="brand" href="#">Project name</a>
+	 
+	      <!-- Everything you want hidden at 940px or less, place within here -->
+	      <div class="nav-collapse collapse">
+	        <!-- .nav, .navbar-search, .navbar-form, etc -->
+	      </div>
+	 
+	    </div>
+	  </div>
+	</div>
+  	
+  	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span2">		
+			    <ul class="nav nav-list">
+				  <li class="nav-header">Usuarios</li>
+				  <li><a href="#">Ma. Carolina</a></li>
+				  <li><a href="#">Marcos</a></li>
+				  <li class="nav-header">Categorias</li>
+				  <li><a href="#">Bancos</a></li>
+				  <li><a href="#">Host Web</a></li>
+				  <li><a href="#">Cajero</a></li>
+				</ul>
+    		</div>
+    		
+    		<div class="span10">
+		
+				<div id="tabla">
+			     <table class="table table-bordered">
+				  <thead>
+				    <tr>
+				      <th>Categoria</th>
+				      <th>Aplicaci&oacute;n</th>
+				      <th>Usuario</th>
+				      <th>Clave</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <td>Banco</td>
+				      <td>Frances</td>
+				      <td>28144658</td>
+					  <td>1608</td>
+				    </tr>
+				    <tr>
+				      <td>Banco</td>
+				      <td>Hipotecario</td>
+				      <td>marcosf</td>
+					  <td>cochera2013</td>
+				    </tr>
+				  </tbody>
+				 </table>
+				</div>
+    		</div>
+    	</div>
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/bootstrap.js"></script>
+
+
+  </body>
+</html>
